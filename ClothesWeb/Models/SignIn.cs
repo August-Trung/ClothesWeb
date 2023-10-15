bac97d2c59683940370563678ceb00730545ca38
August-Trung <nguyenminhtrung01082003@gmail.com>
2023-10-15 23:31:12 +0700

    Add ClothesWeb

---
+++ ClothesWeb/Models/SignIn.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClothesWeb.Models
+{
+    public class SignIn
+    {
+        [Key]
+        [Required(ErrorMessage = "Vui lòng nhập Username!")]
+        [Display(Name = "User")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập Password!")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+}
